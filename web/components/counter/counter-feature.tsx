bcf529cd8344115f93198a94403585fa40f824d4
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-02-25 21:43:28 +0000

    fggdfbgfdgfdgfdgdf

--- web/components/counter/counter-feature.tsx
+++ web/components/counter/counter-feature.tsx
@@ -1,25 +1,15 @@
 // components/counter/counter-feature.tsx
-import React, { useState } from 'react';
+
+import React from 'react';
 import { Box, Button, Text } from '@chakra-ui/react';
 
 const CounterFeature: React.FC = () => {
-    use client;
-  
-    const [count, setCount] = useState(0);
-  
-    const increment = () => setCount(count + 1);
-    const decrement = () => setCount(count - 1);
-  
     return (
-      <Box p={4} display="flex" alignItems="center" justifyContent="center" flexDirection="column">
-        <Text fontSize="2xl" mb={4}>Counter: {count}</Text>
-        <Box display="flex" gap={2}>
-          <Button colorScheme="blue" onClick={decrement}>-</Button>
-          <Button colorScheme="blue" onClick={increment}>+</Button>
+        <Box p={4} display="flex" alignItems="center" justifyContent="center" flexDirection="column">
+            <Text fontSize="2xl" mb={4}>This is a dummy component</Text>
+            <Button colorScheme="blue">Dummy Button</Button>
         </Box>
-      </Box>
     );
-  };
+};
 
 export default CounterFeature;
-  
